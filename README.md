7272df3a7238b7ce75a213b5f4f5b8e00d3aa2ec
rlivermorejr <rlivermorejr@gmail.com>
2022-01-25 20:26:07 -0500

    scraper added

--- README.md
+++ README.md
@@ -2,4 +2,4 @@ This is going to be a program that scrapes online retail stores for items. It wi
 in an API.
 
 The goal is to grab everything of each item the user wants to search for, reviews, comments, price, quantity, shipping
-price, etc.
+price, etc. bCf$va-8WG
--- index.js
+++ index.js
@@ -4,10 +4,25 @@ const request = require("request-promise");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const API_KEY = "b8db8f63baa02f5383c2e9dee8cf8be0";
+const BASE_URL = `http://api.scraperapi.com?api_key=${API_KEY}&autoparse=true`;
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
 	res.send("Welcome to the Scraper API");
+});
+
+// Get Product Details
+app.get("/products/:productId", async (req, res) => {
+	const { productId } = req.params;
+
+	try {
+		const response = await request(`${BASE_URL}&url=https://www.amazon.com/dp/${productId}`);
+		res.json(JSON.parse(response));
+	} catch (error) {
+		res.json(error);
+	}
 });
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
